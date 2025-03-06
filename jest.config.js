751b24d7b3d3974ec435cba97def51cfd8d4b441
Ali Morshedzadeh <amorshed051@gmail.com>
2025-03-06 20:14:48 +0330

    Simplify Jest configuration by disabling coverage

--- jest.config.js
+++ jest.config.js
@@ -3,23 +3,7 @@ module.exports = {
     testEnvironment: 'jsdom',
     testMatch: ['**/*.test.ts', '**/*.test.tsx'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    collectCoverageFrom: [
-        'src/**/*.{ts,tsx}',
-        '!src/tests/**',
-        '!**/node_modules/**',
-    ],
-    coverageThreshold: {
-        global: {
-            branches: 40,
-            functions: 40,
-            lines: 40,
-            statements: 40,
-        },
-    },
-    setupFilesAfterEnv: [],
-    coverageReporters: ['text', 'lcov'],
+    collectCoverage: false,
     testPathIgnorePatterns: ['/node_modules/'],
     transform: {
         '^.+\\.(ts|tsx)$': ['ts-jest', {
